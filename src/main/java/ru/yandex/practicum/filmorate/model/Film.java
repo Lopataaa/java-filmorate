14e525bff60c11ac40082992a76d53cf9cd02786
User Oksana <oks321mih@yandex.ru>
2025-10-18 01:37:43 +0300

    Checkstyle для новых классов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,8 +9,8 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private Integer id;
-    private  String name;
-    private  String description;
+    private String name;
+    private String description;
     private LocalDate releaseDate;
     private Integer duration;
 
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -7,20 +7,48 @@ import java.time.LocalDate;
 @Data
 public class User {
     private Integer id;
-    private  String email;
-    private  String login;
+    private String email;
+    private String login;
     private String name;
     private LocalDate birthday;
 
-    public Integer getId() { return id; }
-    public String getEmail() { return email; }
-    public String getLogin() { return login; }
-    public String getName() { return name; }
-    public LocalDate getBirthday() { return birthday; }
+    public Integer getId() {
+        return id;
+    }
 
-    public void setId(Integer id) { this.id = id; }
-    public void setEmail(String email) { this.email = email; }
-    public void setLogin(String login) { this.login = login; }
-    public void setName(String name) { this.name = name; }
-    public void setBirthday(LocalDate birthday) { this.birthday = birthday; }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 }
