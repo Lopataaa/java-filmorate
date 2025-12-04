4013c3373a689272e2e5effecb7a2bc11569400b
User Oksana <oks321mih@yandex.ru>
2025-12-04 23:46:05 +0300

    Лишний импорт удален

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.film.MpaDbStorage;
 
 import java.util.List;
 
